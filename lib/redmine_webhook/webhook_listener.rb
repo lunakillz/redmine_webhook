75ce28a028ed5a7411214d05ac6ba5c46bd169b7
Logan Song <lunakillz@gmail.com>
2020-07-16 13:57:10 +0900

    add metions & extract_usernames funtions

--- lib/redmine_webhook/webhook_listener.rb
+++ lib/redmine_webhook/webhook_listener.rb
@@ -117,6 +117,23 @@ module RedmineWebhook
         }
       }.to_json
     end
+	  
+    def mentions text
+	return nil if text.nil?
+	names = extract_usernames text
+	names.present? ? "\nTo: " + names.join(', ') : nil
+    end
+
+    def extract_usernames text = ''
+	if text.nil?
+		text = ''
+	end
+
+	# slack usernames may only contain lowercase letters, numbers,
+	# dashes and underscores and must start with a letter or number.
+	text.scan(/@[a-z0-9][a-z0-9_\-]*/).uniq
+    end
+
 
     def post(webhooks, request_body)
       Thread.start do
